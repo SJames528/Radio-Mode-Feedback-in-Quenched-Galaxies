613c3df11f582effb800733813578619d557a66f
SJames528 <hardingsamuel97@gmail.com>
2025-08-06 17:11:26 +0100

    pre-overhaul of coordinate & skycoord system. Progress on random stack functionality

--- library/utils.py
+++ library/utils.py
@@ -8,11 +8,9 @@ import matplotlib.pyplot as plt
 def snap_data(df):
     return np.array([i for i in df["snapshot"]])
 
-#takes a dataframe of points and returns an array of their pixel positions
+#takes a dataframe of points and adds new columns with their pixel positions
 def df_coords_to_pixels(df, coord_system, coord_names, new_names=["pix_x","pix_y"]):
-    pix_coords = []
-    for skypoint in df[coord_names].values:
-        pix_coords.append(coord_system.world_to_pixel(astropy.coordinates.SkyCoord(skypoint[0],skypoint[1],unit="deg")))
+    pix_coords = [coord_system.world_to_pixel(astropy.coordinates.SkyCoord(skypoint[0],skypoint[1],unit="deg")) for skypoint in df[coord_names].values]
     df.loc[:,new_names[0]] = [i[0] for i in pix_coords]
     df.loc[:,new_names[1]] = [i[1] for i in pix_coords] 
     return df
@@ -55,6 +53,8 @@ def pixel_to_snapshot(coord, mosaic, s, pad=True):
 
     df_cut = np.array(image_data[max(up_cutoff,0):down_cutoff, max(left_cutoff,0):right_cutoff])
 
+    df_cut = np.nan_to_num(df_cut, nan=0)
+
     if pad and df_cut.shape != (s,s):
         df_cut_list = df_cut.tolist()
         left_padding = max(0-left_cutoff,0) * [0]
@@ -69,26 +69,29 @@ def pixel_to_snapshot(coord, mosaic, s, pad=True):
     
     return df_cut
 
-#calculate the RA/DEC range for the current mosaic
+#calculate the RA/DEC range for the current mosaic (assumed square)
 def mosaic_dim_limits(mos):
     head_ = mos[0].header
-    ra_min = mos[0].header["CRVAL1"] + mos[0].header["CRPIX1"]*mos[0].header["CDELT1"]
-    ra_max = mos[0].header["CRVAL1"] - mos[0].header["CRPIX1"]*mos[0].header["CDELT1"]
-    dec_min = mos[0].header["CRVAL2"] - mos[0].header["CRPIX2"]*mos[0].header["CDELT2"]
-    dec_max = mos[0].header["CRVAL2"] + mos[0].header["CRPIX2"]*mos[0].header["CDELT2"]
+    ra_min = head_["CRVAL1"] + (head_["NAXIS1"]-head_["CRPIX1"])*head_["CDELT1"]
+    ra_max = head_["CRVAL1"] - head_["CRPIX1"]*head_["CDELT1"]
+    dec_min = head_["CRVAL2"] - head_["CRPIX2"]*head_["CDELT2"]
+    dec_max = head_["CRVAL2"] + (head_["NAXIS2"]-head_["CRPIX2"])*head_["CDELT2"]
     return ra_min, ra_max, dec_min, dec_max
 
 #produce plots of mosaic cutouts
-def visualise(arr, fig_size=(10,10)):
+def visualise(arr, title="", fig_size=(10,10)):
     if isinstance(arr, pd.core.frame.DataFrame):
-        arr = np.array(arr["snapshot"])
-    if len(arr)==0 or not isinstance(arr, (list,np.ndarray)):
+        arr = np.array(arr['snapshot'])
+    if len(arr)==0 or not isinstance(arr, (list,np.ndarray,pd.core.series.Series)):
         raise Exception("Data empty, or not provided as list/array")
     while arr.shape[0]==1:
         arr = arr[0]
     if len(arr.shape)==2:
+        arr = arr * 1000
         plt.imshow(arr,cmap='grey')
         plt.axis('off')
+        plt.colorbar(label="mJy/beam")
+        plt.title(title)
     else:
         fig, ax = plt.subplots(int(0.5+len(arr)/2),2,figsize=fig_size)
         for index, cutout in enumerate(arr):
@@ -97,39 +100,81 @@ def visualise(arr, fig_size=(10,10)):
             subfig.set_axis_off()
         if len(arr)%2:
             ax[-1,-1].axis('off')
+        fig.suptitle(title)
     plt.show()
 
 #plot the image data for all catalogue points in the current mosaic
-def snapshots(df, mos, s=15, coord_names=["RA","DEC"], vis=False, vis_figsize=(10,10)):
+def snapshots(df, mosaics, s=15, coord_names=["RA","DEC"], vis=False, vis_figsize=(10,10)):
     
     """Produces cutouts from a given mosaic centred at points given in a dataframe. The function will select only those objects within the mosaic's (assumed square) field, so the dataframe need not be pre-processed to contain only relevant sources. Size of cutout region can be specified. Coord names are assumed to be "RA" and "DEC", but these can be specified.
 
     :param df: dataframe OR list - the dataframe or list of tuples containing catalogue points to display. In the case of dataframe, columns for right ascension (RA) and declination (DEC) are required, but other contents are not required. If a list is entered, will be first converted to an appropriate dataframe.
-    :param mos: mosaic object - usually loaded in from a fits mosaic with fits.open(file). Mosaic is assumed to be square (that is, all objects within the RA and DEC bounds will attemt to be plotted.
+    :param mos: mosaic object or list of objects - usually loaded in from a fits mosaic with fits.open(file). Mosaic is assumed to be square (that is, all objects within the RA and DEC bounds will attemt to be plotted.
     :param s: int - size of the region (s x s square) to display, with the object position at the centre pixel, or above-left of centre in the case of even integer s value. Default value s=15.
     :param coord_names: [str, str] list - the column names for RA and DEC respectively in the provided dataframe. Default value coord_names=["RA","DEC"].
     """
 
     if isinstance(df, list):
-        df = pd.DataFrame(df)
-        df.columns = coord_names
+        df = pd.DataFrame(df); df.columns = coord_names
+    if isinstance(mosaics, astropy.io.fits.hdu.hdulist.HDUList):
+        mosaics = [mosaics]
+
+    ret = pd.DataFrame([])
+    for mos in mosaics:
+        coord_sys = WCS(mos[0].header)
+        #ra_min, ra_max, dec_min, dec_max = mosaic_dim_limits(mos)
+        if not all([a in df.columns for a in ["pix_x", "pix_y"]]):
+            points = df_coords_to_pixels(df.copy(), coord_sys, coord_names)
+            points = points[((0<=points["pix_x"])&(points["pix_x"]<coord_sys.pixel_shape[0]))&((0<=points["pix_y"])&(points["pix_y"]<coord_sys.pixel_shape[1]))]
+
+            if len(points) == 0:
+                ## TODO: fix when exception is raised when passing only one point but multiple mosaics, since there is a mosaic which it isn't part of.
+                raise Exception("Empty intersection with mosaic")
+            points = df_coords_to_pixels(points, coord_sys, coord_names)
+        else:
+            points = df.copy()
+
+        points["snapshot"] = [pixel_to_snapshot(point, mos, s) for point in points[["pix_x", "pix_y"]].values]
         
-    coord_sys = WCS(mos[0].header)
-    ra_min, ra_max, dec_min, dec_max = mosaic_dim_limits(mos)
-    points = df[(ra_min<=df[coord_names[0]]) & (df[coord_names[0]]<=ra_max) & (dec_min<=df[coord_names[1]]) & (df[coord_names[1]]<=dec_max)].copy()
-    
-    if len(points) == 0:
-        raise Exception("Empty intersection with mosaic")
-    points = df_coords_to_pixels(points, coord_sys, coord_names)
-    
-    ret = []
-    for point in points[["pix_x", "pix_y"]].values:
-        ret.append(pixel_to_snapshot(point, mos, s))
-    points["snapshot"] = ret
+        ret = pd.concat([ret,points])
+        
+    ret = ret[np.array([not not np.count_nonzero(i) for i in ret["snapshot"]])]
 
-    if vis: visualise(points, figsize = vis_figsize)
-    return points
+    if vis: visualise(ret, figsize = vis_figsize)
+    return ret
 
-def stack(cutout_arr, weight_method):
-    
-    return
+def stack(df, weight_method="IVW"):
+    avg_snap = np.zeros(snap_data(df)[0].shape)
+    inv_var_sum = 0
+    for mat in snap_data(df):
+        var = np.std(mat)**2
+        if not var:
+            continue
+        weighted_cutout = mat/var
+        avg_snap += weighted_cutout
+        inv_var_sum += 1/var
+    return avg_snap/inv_var_sum
+
+def skycoord_in_survey_region(point):
+    ra, dec = point
+    #is coord in ra/dec bounds?
+    if not (162.5<ra<237.5 and 46<dec<61.25): return False
+    #is coord within survey shape?
+    return ((170<ra<225 and 46<dec<61.25) or
+            (225<ra<237.5 and 46<dec<56.5) or
+            (225<ra<229.5 and 56.5<dec<58.8) or
+            (162.5<ra<170 and 46<dec<48.5) or
+            (167.5<ra<170 and 48.5<dec<58.75) or
+            (ra<167.5 and dec>48.5 and (9*dec<(22*ra)-3199)))
+
+def random_stack(rng, mos, len_, s=25):
+    random_coord_dict = {'RA': [], 'DEC':[]}
+    while len(random_coord_dict['RA'])<len_:
+        new_rand_point = rng.random(2)*(75,15.25) + (162.5,46)
+        if skycoord_in_survey_region(new_rand_point):
+            random_coord_dict['RA'].append(new_rand_point[0])
+            random_coord_dict['DEC'].append(new_rand_point[1])
+    random_coord_df = snapshots(pd.DataFrame(random_coord_dict), mos, s)
+    random_coord_df = random_coord_df[~random_coord_df.index.duplicated()]
+    random_coord_stack = stack(random_coord_df)
+    return random_coord_stack
--- test.py
+++ test.py
@@ -13,9 +13,11 @@ matched_catalogue = fits.open(data_folder + 'matched_lofar_sdss')
 mat_cat_df = Table(matched_catalogue[1].data).to_pandas()
 
 ##read in mosaic
-#mosaic = fits.open(data_folder+'/mosaics/p164+47-mosaic.fits')
+mosaic_small = fits.open(data_folder+'/mosaics/p164+47-mosaic.fits')
 #mosaic = fits.open(data_folder+'/mosaics/p176+60-mosaic.fits')
-mosaic = fits.open(data_folder+'/mosaics/p169+55-mosaic.fits')
+#mosaic = fits.open(data_folder+'/mosaics/p169+55-mosaic.fits')
+mosaic = fits.open(data_folder+'/mosaics/mosaic-i_1.fits')
+mosaic2 = fits.open(data_folder+'/mosaics/mosaic-i_2.fits')
 
 ##visualise all points in the DF which lie within the selected mosaic
 #vis_data = snapshots(mat_cat_df, mosaic, s=15, coord_names=["RA_1","DEC_1"])
@@ -23,7 +25,7 @@ mosaic = fits.open(data_folder+'/mosaics/p169+55-mosaic.fits')
 
 ##Read in entire quenched catalogue
 quenched_cat = fits.open(data_folder + 'SDSS_quenched_in_skyarea')
-quenched_df = Table(quenched_cat[1].data).to_pandas().drop(columns = ["Source_S"])
+quenched_df = Table(quenched_cat[1].data).to_pandas().drop(columns = ["Source_S","Z","Z_ERR","LGM_TOT_P50","SFR_TOT_P50","SSFR"])
 quenched_df["combined_coord"] = [item for item in zip(quenched_df["RA"],quenched_df["DEC"])]
 
 ##Keep record of matched radio objects
@@ -31,39 +33,116 @@ SDSS_matched_coords = [item for item in zip(mat_cat_df["RA_2"],mat_cat_df["DEC_2
 
 ##Retain only quenched sources which are not radio sources
 quenched_no_radio_df = quenched_df[~quenched_df["combined_coord"].isin(SDSS_matched_coords)]
+quenched_no_radio_df = quenched_no_radio_df.drop(columns=["combined_coord"])
 
-snap_info_quiet = snapshots(quenched_no_radio_df, mosaic, s=25)
-snap_info_loud = snapshots(mat_cat_df, mosaic, s=25, coord_names=["RA_1","DEC_1"])
+snap_info_quiet = snapshots(quenched_no_radio_df, [mosaic, mosaic2], s=25)
+snap_info_loud = snapshots(mat_cat_df, [mosaic, mosaic2], s=25, coord_names=["RA_1","DEC_1"])
 
-#plot histograms of snapshot pixel values
+##See what stacking looks like on radio-loud galaxies, as an example of a positive result
+if False:
+    stack_loud = stack(snap_info_loud)
+    visualise(stack_loud)
+
+##Plot some histograms of typical snapshot pixel values
 if False:
     fig, ax = plt.subplots(2,1,figsize=(10,10))
-    ax[0].hist(snap_info_quiet[0].flatten(), bins=np.linspace(np.min(snap_info_loud[0]), np.max(snap_info_loud[0]), 50))
-    ax[0].set_title("Histogram of pixel values for radio-quiet quenched galaxy"); ax[0].set_xlabel("Pixel value"); ax[0].set_ylabel("Count")
-    ax[1].hist(snap_info_loud[0].flatten(), bins=np.linspace(np.min(snap_info_loud[0]), np.max(snap_info_loud[0]), 50))
-    ax[1].set_title("Histogram of pixel values for radio-loud quenched galaxy"); ax[1].set_xlabel("Pixel value"); ax[1].set_ylabel("Count")
+    ax[0].hist(snap_data(snap_info_quiet[:100]).flatten(), bins=np.linspace(np.min(snap_data(snap_info_loud)), np.max(snap_data(snap_info_loud)), 50), log=True)
+    ax[0].set_title("Histogram of pixel values for first 100 radio-quiet quenched galaxies"); ax[0].set_xlabel("Pixel value"); ax[0].set_ylabel("Count")
+    ax[1].hist(snap_data(snap_info_loud).flatten(), bins=np.linspace(np.min(snap_data(snap_info_loud)), np.max(snap_data(snap_info_loud)), 50), log=True)
+    ax[1].set_title("Histogram of pixel values for radio-loud quenched galaxies"); ax[1].set_xlabel("Pixel value"); ax[1].set_ylabel("Count")
     plt.show()
 
+##1 - Duplicated points (appear on both mosaics). TODO: decide whether to delete duplicates after, or put a condition in the snapshot() function which checks if that index already exists with a snapshot
+duplicated_points = snap_info_quiet[snap_info_quiet.index.duplicated(keep=False)]
+snap_info_quiet = snap_info_quiet[~snap_info_quiet.index.duplicated()]
+if False:
+    print(f'Are these duplicated points present exactly twice? {np.all(duplicated_points.index.value_counts()==2)}')
+    print(f'Number of duplicated points: {int(len(duplicated_points)/2)}')
+    plt.scatter(duplicated_points["RA"], duplicated_points["DEC"]); plt.title("Points appearing on both mosaics"); plt.show()
 
-mosaic_1 = fits.open(data_folder+'/mosaics/p164+47-mosaic.fits')
-mosaic_2 = fits.open(data_folder+'/mosaics/p176+60-mosaic.fits')
-mosaic_3 = fits.open(data_folder+'/mosaics/p169+55-mosaic.fits')
+    duplicated_differences = pd.DataFrame([])
+    duplicated_differences.index = list(set(duplicated_points.index))
+    duplicated_differences["diffs"] = [np.linalg.norm(a-b) for a, b in [duplicated_points.loc[i]["snapshot"] for i in duplicated_differences.index]]
 
-for index, mat in enumerate(snap_data(snap_info_quiet)):
-    if not index:
-        avg_snap = mat
-    else:
-        avg_snap += mat
-visualise(avg_snap)
+    plt.scatter([i for i in range(0,len(duplicated_differences))],duplicated_differences["diffs"]); plt.title("Euclidean distance of separation over the set of duplicated cutouts"); plt.show()
+    visualise(duplicated_points.loc[duplicated_differences[duplicated_differences["diffs"]>2].index], title="Duplicated cutouts which differ the most from eachother")
+    # -> Appears that there's just a rotational difference? Particuarly clear for brighter cutouts. Noisy cutouts appear less consistent, noise in both is independent?
 
-problematic_snaps = snap_info_quiet[[np.max(i) > 0.1 for i in snap_info_quiet["snapshot"]]]
-visualise(problematic_snaps)
 
-unproblematic_snaps = snap_info_quiet.drop(problematic_snaps.index)
+##2 - Some snapshots contain bright radio sources. These should in theory be dealt with by the variance weighting when stacking, but better safe than sorry.
+mid_vals = snap_info_quiet[[0.03<np.max(i)<0.04 for i in snap_data(snap_info_quiet)]]
+if False:
+    visualise(mid_vals[:10], title="")
+problematic_snaps = snap_info_quiet[[np.max(i) > 0.03 for i in snap_data(snap_info_quiet)]]
+snap_info_quiet = snap_info_quiet.drop(problematic_snaps.index)
 
-for index, mat in enumerate(snap_data(unproblematic_snaps)):
-    if not index:
-        avg_snap = mat
-    else:
-        avg_snap += mat
-visualise(avg_snap)
+##3 - Some snapshots have naturally higher baseline signal (due to being too close to bright sources or other noise artifacts). Need some way to subtract the baseline, or remove those with high median values
+if False:
+    low_median_unproblematic = snap_info_quiet[[np.median(i)<0.01 for i in snap_data(snap_info_quiet)]]
+
+##Unused data - fixed with updates to utils.py (no longer pre-filtering, using all skycoords which have valid pixel positions)
+if False:
+    unused = quenched_no_radio_df[[index not in snap_info_quiet.index for index in quenched_no_radio_df.index]]
+    plt.scatter(unused["RA"], unused["DEC"]); plt.show()
+
+##Stacking analysis on filtered dataset
+stack_data = stack(snap_info_quiet)
+visualise(stack_data, title="Stacked cutout on processed dataset")
+
+
+##Try a random stack to compare - need to rework to use random RA/DEC not pixel position
+
+random_coord_dict = {'RA': [], 'DEC': []}
+rng = np.random.default_rng(123)
+while len(random_coord_dict['RA'])<len(snap_info_quiet):
+    new_rand_point = rng.random(2)*(75,15.25) + (162.5,46)
+    if skycoord_in_survey_region(new_rand_point):
+        random_coord_dict['RA'].append(new_rand_point[0])
+        random_coord_dict['DEC'].append(new_rand_point[1])
+random_coord_df = pd.DataFrame(random_coord_dict)
+
+random_coord_snaps = snapshots(random_coord_df, [mosaic,mosaic2], s=25)
+#Visual of duplicated region:
+if True:
+    dup_points = random_coord_snaps[random_coord_snaps.index.duplicated(keep=False)]
+    plt.scatter(random_coord_snaps["RA"], random_coord_snaps["DEC"], s=2)
+    plt.scatter(dup_points["RA"], dup_points["DEC"], s=2)
+    plt.title("Mosaic overlap region - points in the intersection have two distinct data cutouts available")
+    plt.show()
+
+random_coord_snaps = random_coord_snaps[~random_coord_snaps.index.duplicated()]
+random_coord_stack = stack(random_coord_snaps)
+visualise(random_coord_stack, title="Stacked random points in the mosaics")
+
+##100 stacked cutouts of random populations:
+rng = np.random.default_rng()
+len_random = 100
+if False:
+    random_snapshots = np.array([random_stack(rng, [mosaic,mosaic2], len_=len(snap_info_quiet)) for i in range(len_random)])
+    with open("100_random_stacks.npy", 'wb') as f:
+        np.save(f, random_snapshots)
+else:  
+    with open("100_random_stacks.npy", 'rb') as f:
+        random_snapshots = np.load(f)
+
+##Histograms of pixel values:
+if False:
+    plt.hist(stack_data.flatten()*1000, bins=np.linspace(-0.1,0.4,num=100))
+    plt.hist(random_coord_stack.flatten()*1000, bins=np.linspace(-0.1,0.4,num=100))
+    plt.title("Historgram of pixel values for stacked galaxies vs random cutouts")
+    plt.show()
+#look into why most snaps have positive mean but stacked the mean is negative
+if False:
+    plt.hist([random_coord_stack.flatten()*100,snap_data(random_coord_snaps)[:100].flatten()], log=True, alpha=0.2, bins=np.linspace(-0.02,0.05,num=100))
+    plt.show()
+
+##Angular separation analysis
+#add column for skycoord() for each object
+#snap_info_quiet["RA"]-snap_info_quiet["RA"].loc[0]
+#keep only less than mod 1
+#search skycoord.separation() over remaining
+#save lowest number
+#repeat over set
+
+
+
